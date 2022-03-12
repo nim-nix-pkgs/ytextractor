1c92f1265fb5ff71d86efac5945df0b789430442
hugosenari <hugosenari@gmail.com>
2022-03-11 23:31:57 -0300

    chore: re index {nameLo}

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Youtube data extractor'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."ytextractor-master".url = "path:./master";
+  inputs."ytextractor-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."ytextractor-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Youtube data extractor'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-ytextractor-master.flake = false;
+  inputs.src-ytextractor-master.owner = "thisago";
+  inputs.src-ytextractor-master.ref   = "refs/heads/master";
+  inputs.src-ytextractor-master.repo  = "ytextractor";
+  inputs.src-ytextractor-master.type  = "github";
+  
+  inputs."ajax".dir   = "nimpkgs/a/ajax";
+  inputs."ajax".owner = "riinr";
+  inputs."ajax".ref   = "flake-pinning";
+  inputs."ajax".repo  = "flake-nimble";
+  inputs."ajax".type  = "github";
+  inputs."ajax".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."ajax".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-ytextractor-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-ytextractor-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
